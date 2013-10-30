06211f42140a050d9687397e42f7245f9a7fec0b
Brad Chapman <chapmanb@50mail.com>
2013-10-30 09:39:46 -0500

    Move sambamba to 0.4.1. Adds index stats and fixes high-load issues: lomereiter/sambamba#56

--- sambamba.rb
+++ sambamba.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Sambamba < Formula
   homepage 'https://github.com/lomereiter/sambamba'
-  version '0.4.0'
-  url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.0/sambamba_v0.4.0_centos5.tar.bz2'
-  sha1 '3ee6ffce2d9e784dbbf6535e47cefdedf3406176'
+  version '0.4.1'
+  url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.1/sambamba_v0.4.1b_centos5.tar.bz2'
+  sha1 '7b7c4173133c65848086964c07fd980d75df136a'
 
   def install
     raise 'sambamba not yet supported for MacOSX' if OS.mac?
-    bin.install 'sambamba_v0.4.0' => 'sambamba'
+    bin.install 'sambamba_v0.4.1b' => 'sambamba'
   end
 
   test do
