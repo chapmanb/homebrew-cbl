d9e727331b2d96d81e9b06ee15a71b7cd513ae26
lomereiter <lomereiter@gmail.com>
2013-12-18 21:06:38 +0400

    bump sambamba version

--- sambamba.rb
+++ sambamba.rb
@@ -2,18 +2,18 @@ require 'formula'
 
 class Sambamba < Formula
   homepage 'https://github.com/lomereiter/sambamba'
-  version '0.4.3'
+  version '0.4.4'
 
   if OS.mac?
-    url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.3/sambamba_v0.4.3_osx.tar.bz2'
-    sha1 'f0ca3c03a1a17673c35fa5e03665fce3fdc8f3b8'
+    url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.4/sambamba_v0.4.4_osx.tar.bz2'
+    sha1 '2eb39b5c8100dca487f3d288f90dfc7cb2a8e4d3'
   else
-    url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.3/sambamba_v0.4.3_centos5.tar.bz2'
-    sha1 'd181e952c9d63ac7391fbb9a5ba4119d24e1c69d'
+    url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.4/sambamba_v0.4.4_centos5.tar.bz2'
+    sha1 '1dde69de12475fb6e5cf6dd1ed9392d73091bedd'
   end
 
   def install
-    bin.install 'sambamba_v0.4.3' => 'sambamba'
+    bin.install 'sambamba_v0.4.4' => 'sambamba'
   end
 
   test do
