daad3445b888bcc3f9ad648abcafff2e9eb2ef67
chapmanb <chapmanb@50mail.com>
2014-04-24 13:51:40 -0400

    Bump vt to recent version for testing #8

--- vt.rb
+++ vt.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Vt < Formula
   homepage 'https://github.com/atks/vt'
-  version '2014-04-09'
-  url 'https://github.com/atks/vt.git', :revision => 'a7c50713594'
+  version '2014-04-23'
+  url 'https://github.com/atks/vt.git', :revision => '22894f949a'
 
   def install
     system 'make'
