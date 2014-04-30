da698e20d1b63ba943600af45742016d6e8ec3c8
chapmanb <chapmanb@50mail.com>
2014-04-30 05:42:34 -0400

    Update sambamba to latest release

--- sambamba.rb
+++ sambamba.rb
@@ -2,24 +2,20 @@ require 'formula'
 
 class Sambamba < Formula
   homepage 'https://github.com/lomereiter/sambamba'
-
+  version '0.4.6'
   if OS.mac?
-    url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.4/sambamba_v0.4.4_osx.tar.bz2'
-    sha1 '2eb39b5c8100dca487f3d288f90dfc7cb2a8e4d3'
+    url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.6/sambamba_v0.4.6_osx.tar.bz2'
+    sha1 'e92ae523a56c2754d731aabf5854e23030f466b7'
   else
-    url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.6-beta/sambamba_v0.4.6-beta_centos5-x86_64.tar.bz2'
-    sha1 'afd590981b8050867cc3c8d80fed44135a600519'
+    url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.6/sambamba_v0.4.6_centos5.tar.bz2'
+    sha1 'c7679176877325a3c83998fa1e6095dfe94b42ee'
   end
 
   def install
-    if OS.mac?
-      bin.install 'sambamba_v0.4.4' => 'sambamba'
-    else
-      bin.install 'sambamba_v0.4.6' => 'sambamba'
-    end
+    bin.install 'sambamba_v0.4.6' => 'sambamba'
   end
 
   test do
-    system 'sambamba', 'index'
+    system 'sambamba index'
   end
 end
