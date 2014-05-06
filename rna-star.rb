859b1219f9cf434389384bed5fb39b7d88143d50
roryk <rory.kirchner@gmail.com>
2014-05-06 10:10:32 -0400

    Fix shasum for rna-star.

--- rna-star.rb
+++ rna-star.rb
@@ -4,7 +4,7 @@ class RnaStar < Formula
   homepage 'https://code.google.com/p/rna-star'
   version '2.3.1z3'
   url 'http://it-collab01.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARreleases/Patches/STAR_2.3.1z.tgz'
-  sha1 '843e66d8e4f3e8119596dbc15cdd30c65f43907e'
+  sha1 'a8a5e5a6d9475e0558f1ee66333770fd4ed87ceb'
 
   def install
     bin.install 'STAR'
