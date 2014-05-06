88223f02e533ebb6343a118ea6c2900e9e0e9642
roryk <rory.kirchner@gmail.com>
2014-05-06 00:51:42 -0400

    Downgrade rna-star to 2.3.1z.
    2.3.1z3 has a bug making it unusable.
    
    https://groups.google.com/forum/#!topic/rna-star/UUtAKwd2vKI

--- rna-star.rb
+++ rna-star.rb
@@ -3,9 +3,7 @@ require 'formula'
 class RnaStar < Formula
   homepage 'https://code.google.com/p/rna-star'
   version '2.3.1z3'
-  # Need latest alpha release to work around 13.04 compile error
-  # https://groups.google.com/forum/#!topic/rna-star/13S344Jknf4
-  url 'http://it-collab01.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARreleases/Patches/STAR_2.3.1z3.tgz'
+  url 'http://it-collab01.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARreleases/Patches/STAR_2.3.1z.tgz'
   sha1 '843e66d8e4f3e8119596dbc15cdd30c65f43907e'
 
   def install
