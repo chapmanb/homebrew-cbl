ec34808d8a39ae9d799c9838d44f33bc52b343e6
roryk <rory.kirchner@gmail.com>
2014-05-08 13:56:07 -0400

    Update rna-star to fixed z4 version.
    z3 had a showstopper issue that got fixed:
    http://groups.google.com/forum/#!topic/rna-star/UUtAKwd2vKI

--- rna-star.rb
+++ rna-star.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class RnaStar < Formula
   homepage 'https://code.google.com/p/rna-star'
-  version '2.3.1z3'
-  url 'http://it-collab01.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARreleases/Patches/STAR_2.3.1z.tgz'
-  sha1 'a8a5e5a6d9475e0558f1ee66333770fd4ed87ceb'
+  version '2.3.1z4'
+  url 'http://it-collab01.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARreleases/Patches/STAR_2.3.1z4.tgz'
+  sha1 'e46fdf32e6305c4120ced2b62db073012bebf4ad'
 
   def install
     bin.install 'STAR'
