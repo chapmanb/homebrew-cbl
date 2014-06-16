3947e5b6c63322251f1d9a75006b5f161324e01b
chapmanb <chapmanb@50mail.com>
2014-06-16 05:57:09 -0400

    Update scalpel to latest revision with complex variant fixes chapmanb/bcbio-nextgen#432

--- scalpel.rb
+++ scalpel.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Scalpel < Formula
   homepage 'http://scalpel.sourceforge.net/'
-  version '0.1.1-beta-2014-05-27'
-  url 'http://git.code.sf.net/p/scalpel/code.git', :revision => '3c3f201e4'
+  version '0.1.1-beta-2014-06-15'
+  url 'http://git.code.sf.net/p/scalpel/code.git', :revision => 'e2eaa3582'
 
   patch do
     # Use samtools by chromosome to avoid pulling the full genome into memory
-    url "https://gist.githubusercontent.com/chapmanb/9248ab8409a473249a07/raw/07bfc4e97a7a784b4e0b2d268e5d61a263487907/samtools_index.patch"
-    sha1 'e404cb5de68fffbfa4faed6dbd586d43172ce11e'
+    url 'https://gist.githubusercontent.com/chapmanb/9248ab8409a473249a07/raw/61af40ea24ad8dedf00efb19003bc44dd7436baa/samtools_index.patch'
+    sha1 'd23aa6a8e9b68ab0daaa9976152c0500d9b912a8'
   end
 
   def install
@@ -23,6 +23,8 @@ class Scalpel < Formula
       s.sub! 'system($cmd)', 'system("/bin/bash -c \'$cmd\'")'
     end
     inreplace 'Makefile', 'CXX := g++', "CXX := #{ENV.cxx}"
+    chmod 0755, 'FindVariants.pl'
+    chmod 0755, 'FindSomatic.pl'
     system 'make bamtools'
     system 'make Microassembler'
     system 'make'
