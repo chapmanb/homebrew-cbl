86615edc26a5945799d498124c138c1f7bf7952a
chapmanb <chapmanb@50mail.com>
2014-06-21 06:43:20 -0400

    Update to latest version of sambamba fixing edge cases with BAM output: lomereiter/sambamba#79 chapmanb/bcbio-nextgen#445

--- sambamba.rb
+++ sambamba.rb
@@ -2,17 +2,17 @@ require 'formula'
 
 class Sambamba < Formula
   homepage 'https://github.com/lomereiter/sambamba'
-  version '0.4.6'
+  version '0.4.7'
   if OS.mac?
-    url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.6/sambamba_v0.4.6_osx.tar.bz2'
-    sha1 'e92ae523a56c2754d731aabf5854e23030f466b7'
+    url "https://github.com/lomereiter/sambamba/releases/download/v#{version}/sambamba_v#{version}_osx.tar.bz2"
+    sha1 'f2109903023ea0006d94224f3823a2353abe1809'
   else
-    url 'https://github.com/lomereiter/sambamba/releases/download/v0.4.6/sambamba_v0.4.6_centos5.tar.bz2'
-    sha1 'c7679176877325a3c83998fa1e6095dfe94b42ee'
+    url "https://github.com/lomereiter/sambamba/releases/download/v#{version}/sambamba_v#{version}_centos5.tar.bz2"
+    sha1 '0191c4dd99d40516e912f19725b6115ba4622fd0'
   end
 
   def install
-    bin.install 'sambamba_v0.4.6' => 'sambamba'
+    bin.install "sambamba_v#{version}" => 'sambamba'
   end
 
   test do
