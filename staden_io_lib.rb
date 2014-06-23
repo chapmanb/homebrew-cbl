3314a42944d3e69cfc636cbdf51b5cf047aa0a52
chapmanb <chapmanb@50mail.com>
2014-06-23 05:22:56 -0400

    Update scramble to latest version

--- staden_io_lib.rb
+++ staden_io_lib.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class StadenIoLib < Formula
   homepage 'http://staden.sourceforge.net/'
-  url 'http://downloads.sourceforge.net/project/staden/io_lib/1.13.5/io_lib-1.13.5.tar.gz'
-  sha1 '0b8c99b3a62d1c8f19e4f1ab6b0aad9960568599'
+  version '1.13.7'
+  url "http://downloads.sourceforge.net/project/staden/io_lib/#{version}/io_lib-#{version}.tar.gz"
+  sha1 '4f96f7e25f58d7396e53a66b235a37b55e622f41'
 
   def install
     system './configure', "--prefix=#{prefix}"
