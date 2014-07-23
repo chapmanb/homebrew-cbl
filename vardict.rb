818e35df0d06fc948f80cce0e6284ae5f01ed894
chapmanb <chapmanb@50mail.com>
2014-07-23 06:26:15 -0400

    Build target for VarDict somatic variant caller (chapmanb/bcbio-nextgen#514)

---
+++ vardict.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Vardict < Formula
+  homepage 'https://github.com/AstraZeneca-NGS/VarDict'
+  version '2014-07-23'
+  url 'https://github.com/AstraZeneca-NGS/VarDict.git', :revision => '0292016ff'
+
+  def install
+    prefix.install Dir['*']
+    bin.install_symlink prefix / 'vardict.pl' => 'vardict'
+    bin.install_symlink(['vardict.pl', 'testsomatic.R', 'teststrandbias.R',
+                         'var2vcf_valid.pl', 'var2vcf_somatic.pl'].map do
+                          |x| prefix / x
+    end)
+  end
+
+  test do
+    system 'vardict -h'
+  end
+end
