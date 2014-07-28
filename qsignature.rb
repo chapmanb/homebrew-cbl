44c0720284dec9bc1d72d5ee8a081e7bc91599fe
Lorena Pantano <lorena.pantano@gmail.com>
2014-07-28 16:14:29 -0400

    reusing version var and using DIR[*jar]

--- qsignature.rb
+++ qsignature.rb
@@ -7,16 +7,8 @@ class Qsignature < Formula
   sha1 'be0e50472221dfb7946e0fdebbe4a817ed7097f9'
 
   def install
-    jarversion = '0.1pre'
     java = share / 'java'
-    java.install "qsignature-#{jarversion}.jar"
-    java.install "qpicard-#{jarversion}.jar"
-    java.install "qio-#{jarversion}.jar"
-    java.install "qcommon-#{jarversion}.jar"
-    java.install "jopt-simple-3.2.jar"
-    java.install "commons-math3-3.1.1.jar"
-    java.install "sam-1.110.jar"
-    java.install "picard-1.110.jar"
+    java.install Dir['*.jar']
     bin.mkdir
     open(bin / 'qsignature', 'w') do |file|
       file.write <<-EOS.undent
@@ -44,7 +36,7 @@ class Qsignature < Formula
         if [ "$jvm_mem_opts" == "" ]; then
             jvm_mem_opts="$default_jvm_mem_opts"
         fi
-        eval java $jvm_mem_opts $jvm_prop_opts -cp #{java}/qsignature-#{jarversion}.jar $pass_args
+        eval java $jvm_mem_opts $jvm_prop_opts -cp #{java}/qsignature-#{version}.jar $pass_args
       EOS
     end
   end
