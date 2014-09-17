ca5e39d61a36b579bff9156618a3571f75ff5abf
chapmanb <chapmanb@50mail.com>
2014-09-17 05:14:16 -0400

    Update pindel build to use lightweight tag from @liangkaiye (fixes genome/pindel#4) and explicitly use samtools 0.1.19 for build. #12

--- pindel.rb
+++ pindel.rb
@@ -3,12 +3,24 @@ require 'formula'
 class Pindel < Formula
   homepage 'http://gmt.genome.wustl.edu/packages/pindel/index.html'
   version '0.2.5a7'
-  url "https://github.com/genome/pindel.git", :revision => '14e910d90'
-  depends_on "samtools" => :build
+  url 'https://github.com/genome/pindel/archive/bcbio.tar.gz'
+  sha1 '6c083730d24ed4097e9f859c24c6a090e202dd3a'
+
+  # Requires samtools 0.1.19
+  # depends_on 'samtools' => :build 
+  resource 'samtools' do
+    url 'https://downloads.sourceforge.net/project/samtools/samtools/0.1.19/samtools-0.1.19.tar.bz2'
+    sha1 'ff3f4cf40612d4c2ad26e6fcbfa5f8af84cbe881'
+  end
+  
   def install
-    samtools =  "#{HOMEBREW_PREFIX}/include/bam" 
+    samtools = prefix / 'samtools'
+    resource('samtools').stage do
+      system 'make'
+      samtools.install Dir['*']
+    end
     system "./INSTALL  #{samtools}"
-    bin.install "pindel" => 'pindel'
+    bin.install %w{pindel pindel2vcf sam2pindel}
   end
 
   test do
