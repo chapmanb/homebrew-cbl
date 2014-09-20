7c54621fa52d49965e77e9f91e4615fa18bfd559
roryk <rory.kirchner@gmail.com>
2014-09-20 16:51:22 -0400

    Update STAR to 2.4.0.c.

--- rna-star.rb
+++ rna-star.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class RnaStar < Formula
   homepage 'https://code.google.com/p/rna-star'
-  version '2.3.1z4'
-  url 'http://it-collab01.cshl.edu/shares/gingeraslab/www-data/dobin/STAR/STARreleases/Patches/STAR_2.3.1z4.tgz'
-  sha1 'e46fdf32e6305c4120ced2b62db073012bebf4ad'
+  version '2.4.0c'
+  url 'https://github.com/alexdobin/STAR/archive/STAR_2.4.0c.tar.gz'
+  sha1 '376bbb9827f608215dedfd423b342ee41ebae129'
 
   def install
     bin.install 'STARstatic' => 'STAR'
