237341c79edc963cfbb8d2beb1e0614cd1987180
roryk <rory.kirchner@gmail.com>
2014-10-20 11:02:21 -0400

    Update to 2.4.0d.
    This addresses chapmanb/bcbio-nextgen#625.
    The issue seemed to be spaces in the read names:
    
    https://groups.google.com/forum/#!topic/rna-star/mlcu7j4bocI

--- rna-star.rb
+++ rna-star.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class RnaStar < Formula
   homepage 'https://code.google.com/p/rna-star'
-  version '2.4.0c'
-  url 'https://github.com/alexdobin/STAR/archive/STAR_2.4.0c.tar.gz'
-  sha1 '376bbb9827f608215dedfd423b342ee41ebae129'
+  version '2.4.0d'
+  url 'https://github.com/alexdobin/STAR/archive/STAR_2.4.0d.tar.gz'
+  sha1 '2e5870a78fcbc3c6662802e13eb7fb649b10c7fd'
 
   def install
     bin.install 'STARstatic' => 'STAR'
