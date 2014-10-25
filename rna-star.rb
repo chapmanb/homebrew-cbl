8b666a6007938d811759c2a0a2888e91262a698e
roryk <rory.kirchner@gmail.com>
2014-10-25 11:34:31 -0400

    Upgrade STAR to 2.4.0e. Add support for OSX.

--- rna-star.rb
+++ rna-star.rb
@@ -2,12 +2,16 @@ require 'formula'
 
 class RnaStar < Formula
   homepage 'https://code.google.com/p/rna-star'
-  version '2.4.0d'
-  url 'https://github.com/alexdobin/STAR/archive/STAR_2.4.0d.tar.gz'
-  sha1 '2e5870a78fcbc3c6662802e13eb7fb649b10c7fd'
+  version '2.4.0e'
+  url 'https://github.com/alexdobin/STAR/archive/STAR_2.4.0e.tar.gz'
+  sha1 '6ad268a774ebb24b8262dd333d68e6efe21bf7bc'
 
   def install
-    bin.install 'STARstatic' => 'STAR'
+    if OS.mac?
+      bin.install 'bin/MacOSX_x86_64/STAR' => 'STAR'
+    else
+      bin.install 'bin/Linux_x86_64_static/STAR' => 'STAR'
+    end
     bin.install_symlink '../libexec/wrappers/trans-abyss'
   end
 
