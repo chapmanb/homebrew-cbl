79ccca9af24992bb2c93fd04ed0b5d8d2182b81d
chapmanb <chapmanb@50mail.com>
2014-10-27 06:36:53 -0400

    gof3r: v0.4.8 with cp command.

--- s3gof3r.rb
+++ s3gof3r.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class S3gof3r < Formula
   homepage 'https://github.com/rlmcpherson/s3gof3r'
-  version '0.4.0'
+  version '0.4.8'
   if OS.mac?
     url "https://github.com/rlmcpherson/s3gof3r/releases/download/v#{version}/gof3r_#{version}_darwin_amd64.zip"
-    sha1 '9ebb0b4fef9b5dd1e90bf39b6d5d1379da8b4c95'
+    sha1 "4b8e6dcc5ae4436874cbff4d62f8adf435e4a8e2"
   else
     url "https://github.com/rlmcpherson/s3gof3r/releases/download/v#{version}/gof3r_#{version}_linux_amd64.tar.gz"
-    sha1 '98f5626407fe1148549a6a8b272f926dc024e13c'
+    sha1 "c2363f30d06928b1461028944fea37281b00ed01"
   end
 
   def install
