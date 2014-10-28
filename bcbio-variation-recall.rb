172407c8135bf950989a546f7e4ef7953d790c00
chapmanb <chapmanb@50mail.com>
2014-10-28 07:21:52 -0400

    bcbio-variation-recall: v0.0.4 with FreeBayes GATK compatibility fixes

--- bcbio-variation-recall.rb
+++ bcbio-variation-recall.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class BcbioVariationRecall < Formula
   homepage "https://github.com/chapmanb/bcbio.variation.recall/"
-  version "0.0.3"
+  version "0.0.4"
   url "https://github.com/chapmanb/bcbio.variation.recall/releases/download/v#{version}/bcbio-variation-recall"
-  sha1 "ce4df60ea423ff431177b4181f30fd8b04ee9c9f"
+  sha1 "d1ca4ed23ac24bad04c828012795180e592d4d06"
 
   def install
     bin.install "bcbio-variation-recall"
