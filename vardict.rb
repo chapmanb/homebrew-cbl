90a79cfa1c4c87f77abeb464d69f86869a4ccf22
Miika Ahdesmaki <miika.ahdesmaki@live.fi>
2014-11-27 09:18:05 +0000

    Correct var2vcf_tumor into var2vcf_somatic

--- vardict.rb
+++ vardict.rb
@@ -9,7 +9,7 @@ class Vardict < Formula
     prefix.install Dir['*']
     bin.install_symlink prefix / 'vardict.pl' => 'vardict'
     bin.install_symlink(['vardict.pl', 'testsomatic.R', 'teststrandbias.R',
-                         'var2vcf_valid.pl', 'var2vcf_paired.pl', 'var2vcf_tumor.pl'].map do
+                         'var2vcf_valid.pl', 'var2vcf_paired.pl', 'var2vcf_somatic.pl'].map do
                           |x| prefix / x
     end)
   end
