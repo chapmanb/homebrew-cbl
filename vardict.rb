c64b0afc3067fc2152c1aef0de79215b7d7a740c
Miika Ahdesmaki <miika.ahdesmaki@live.fi>
2014-11-27 09:12:53 +0000

    Update vardict.rb to use var2vcf_paired.pl ...
    and pull latest updates

--- vardict.rb
+++ vardict.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Vardict < Formula
   homepage 'https://github.com/AstraZeneca-NGS/VarDict'
-  version '2014-11-11'
-  url 'https://github.com/AstraZeneca-NGS/VarDict.git', :revision => '0c5eb8b17'
+  version '2014-11-27'
+  url 'https://github.com/AstraZeneca-NGS/VarDict.git', :revision => 'cd60a2220'
 
   def install
     prefix.install Dir['*']
     bin.install_symlink prefix / 'vardict.pl' => 'vardict'
     bin.install_symlink(['vardict.pl', 'testsomatic.R', 'teststrandbias.R',
-                         'var2vcf_valid.pl', 'var2vcf_somatic.pl'].map do
+                         'var2vcf_valid.pl', 'var2vcf_paired.pl', 'var2vcf_tumor.pl'].map do
                           |x| prefix / x
     end)
   end
