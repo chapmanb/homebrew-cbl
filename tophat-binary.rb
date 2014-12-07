81298f9b97f83d91b7aa89dc75c80fd2d5615bc4
roryk <rory.kirchner@gmail.com>
2014-12-06 22:51:34 -0500

    Update Tophat to 2.0.13.
    Tophat 2.0.13 bundles an older compatible version of samtools so we don't have
    to patch the check anymore. It also fixes a bunch of nasty looking warnings
    that cropped up when it was run with a too-new version of samtools.

--- tophat-binary.rb
+++ tophat-binary.rb
@@ -2,20 +2,14 @@ require 'formula'
 
 class TophatBinary < Formula
   homepage 'http://ccb.jhu.edu/software/tophat'
-  version '2.0.12.1'
+  version '2.0.13'
 
   if OS.mac?
-    url "http://ccb.jhu.edu/software/tophat/downloads/tophat-2.0.12.OSX_x86_64.tar.gz"
-    sha1 '796d22e87aa1fbcbfc667ecab2d4a575b3b8602d'
+    url "http://ccb.jhu.edu/software/tophat/downloads/tophat-2.0.13.OSX_x86_64.tar.gz"
+    sha1 'b40cdc6442089d0cf22387bf18d64abf43fe279f'
   else
-    url "http://ccb.jhu.edu/software/tophat/downloads/tophat-2.0.12.Linux_x86_64.tar.gz"
-    sha1 'e3c4858fe43351dc125b478f83656916966cde41'
-  end
-
-  patch do
-    # Enable support for samtools 1.0 in tophat samtools checks
-    url 'https://gist.githubusercontent.com/chapmanb/b04cccddfe1e18c2b542/raw/a2d5626109454a83c7b0431860b33934d5d29b32/tophat_samtools_10.diff'
-    sha1 'a89741de6405979113e8d8727702bc35d14b69f1'
+    url "http://ccb.jhu.edu/software/tophat/downloads/tophat-2.0.13.Linux_x86_64.tar.gz"
+    sha1 'e313f188b97f143ec7c3a9ed9c7a702e0463ea24'
   end
 
   def install
