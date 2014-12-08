748e13716e9a649f676177ac6bbce7899bc1153e
Miika Ahdesmaki <miika.ahdesmaki@live.fi>
2014-12-08 09:14:23 +0000

    VarDict MNV bug fixes and
    performance updates (10% speedup)

--- vardict.rb
+++ vardict.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Vardict < Formula
   homepage 'https://github.com/AstraZeneca-NGS/VarDict'
-  version '2014-12-02'
-  url 'https://github.com/AstraZeneca-NGS/VarDict.git', :revision => 'e354eceb0'
+  version '2014-12-08'
+  url 'https://github.com/AstraZeneca-NGS/VarDict.git', :revision => '9ffec9168'
 
   def install
     prefix.install Dir['*']
