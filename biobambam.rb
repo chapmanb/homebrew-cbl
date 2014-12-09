89c7ce38ce599055a6acffd7b0585e5a5f153204
chapmanb <chapmanb@50mail.com>
2014-12-09 08:04:40 -0500

    Bump libmaus and biobambam to the latest versions

--- biobambam.rb
+++ biobambam.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Biobambam < Formula
   homepage 'https://github.com/gt1/biobambam'
-  version '0.0.154'
-  url 'https://github.com/gt1/biobambam/archive/0.0.154-release-20140709001615.tar.gz'
-  sha1 'fec4738a871d6d7110ae1e793f598351cf9eda16'
+  version '0.0.182'
+  url 'https://github.com/gt1/biobambam/archive/0.0.182-release-20141201100458.tar.gz'
+  sha1 '799a6e6d80b2044b9b7fcc0fb300cb9d688c27f8'
 
   depends_on 'libmaus'
 
--- libmaus.rb
+++ libmaus.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Libmaus < Formula
   homepage 'https://github.com/gt1/libmaus'
-  version '0.0.147'
-  url 'https://github.com/gt1/libmaus/archive/0.0.147-release-20140714102038.tar.gz'
-  sha1 '4dd3b0d7f481da958f9b5567265965513e2465f7'
+  version '0.0.185'
+  url 'https://github.com/gt1/libmaus/archive/0.0.185-release-20141201090944.tar.gz'
+  sha1 '58997dc25fed89a296fddaf5441cf3985b02a6b8'
 
   depends_on 'pkg-config'
   depends_on 'staden_io_lib'
