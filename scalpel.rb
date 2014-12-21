71faa5a0a363e383403bea5efaf7dd1f6b8a4fc8
chapmanb <chapmanb@50mail.com>
2014-12-21 05:21:44 -0500

    scalpel: upgrade to 0.3.1

--- scalpel.rb
+++ scalpel.rb
@@ -2,21 +2,18 @@ require 'formula'
 
 class Scalpel < Formula
   homepage 'http://scalpel.sourceforge.net/'
-  version '0.2.1'
-  url 'http://downloads.sourceforge.net/project/scalpel/scalpel-0.2.1.tar.gz'
+  version '0.3.1'
+  url 'http://downloads.sourceforge.net/project/scalpel/scalpel-0.3.1.tar.gz'
+  sha1 "b04890373ef730b715f179d434c875c1d5329bd7"
 
   patch do
     # Use samtools by chromosome to avoid pulling the full genome into memory
-    url 'https://gist.githubusercontent.com/chapmanb/9248ab8409a473249a07/raw/61af40ea24ad8dedf00efb19003bc44dd7436baa/samtools_index.patch'
-    sha1 'd23aa6a8e9b68ab0daaa9976152c0500d9b912a8'
+    url "https://gist.githubusercontent.com/chapmanb/9248ab8409a473249a07/raw/c535ff0335470ae599ac7d8f95cee4bc15e67eee/samtools_index.patch"
+    sha1 "e58a688e0dd8be8d7cc3fd47105d20c98b1830d7"
   end
 
   def install
     ENV.deparallelize
-    # Fix bug in HashesIO.pm 0.2.1 release -- submitted upstream
-    inreplace 'HashesIO.pm' do |s|
-      s.sub! 'sget(', 'inTarget('
-    end
     inreplace 'scalpel' do |s|
       # Include PATH to perl libraries
       s.sub! 'use Usage;', "use lib '#{prefix}';\nuse Usage;"
