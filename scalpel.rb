e868e3586af2a842ec7c55c437b57b7ea4e15254
chapmanb <chapmanb@50mail.com>
2015-01-01 05:57:44 -0500

    scalpel: use built-in scalpel tumor/normal filtering which is more effective than our current post-calling filters at removing false positives

--- scalpel.rb
+++ scalpel.rb
@@ -2,15 +2,9 @@ require 'formula'
 
 class Scalpel < Formula
   homepage 'http://scalpel.sourceforge.net/'
-  version '0.3.1_2-14e40c'
+  version '0.3.1_3-14e40c'
   url "https://s3.amazonaws.com/cloudbiolinux/cache/scalpel-code-14e40c09168819c338bcb91c9223378f0147c3f5.zip"
   sha1 "c7a6badbbafe3bbf0aa2fec5219b133f29b2a5fe"
-
-  patch do
-    # Use samtools by chromosome to avoid pulling the full genome into memory
-    url "https://gist.githubusercontent.com/chapmanb/9248ab8409a473249a07/raw/c535ff0335470ae599ac7d8f95cee4bc15e67eee/samtools_index.patch"
-    sha1 "e58a688e0dd8be8d7cc3fd47105d20c98b1830d7"
-  end
 
   patch do
     # Ensure pathlimit argument passed along to somatic and denovo commands
@@ -25,6 +19,10 @@ class Scalpel < Formula
       # Remove incorrect filter specification which causes parsing issues with GATK
       s.sub! "ID=MS,Number=1,Type=STRING", "ID=MS"
       s.sub! "ID=lowCov,Number=0,Type=flag", "ID=lowCov"
+    end
+    inreplace 'FindVariants.pl' do |s|
+      # Use samtools by chromosome to avoid pulling the full genome into memory
+      s.sub! 'if (-e "$bedfile") { $USEFAIDX = 0; }', 'if (-e "$bedfile") { $USEFAIDX = 1; }'
     end
     inreplace 'scalpel' do |s|
       # Include PATH to perl libraries
