9c402bdf1ca5a2da5129240ef633e9ae819b1351
chapmanb <chapmanb@50mail.com>
2015-01-08 10:14:45 -0500

    scalpel: update to latest development that incorporates patches previously in the recipe

--- scalpel.rb
+++ scalpel.rb
@@ -2,24 +2,12 @@ require 'formula'
 
 class Scalpel < Formula
   homepage 'http://scalpel.sourceforge.net/'
-  version '0.3.1_3-14e40c'
-  url "https://s3.amazonaws.com/cloudbiolinux/cache/scalpel-code-14e40c09168819c338bcb91c9223378f0147c3f5.zip"
-  sha1 "c7a6badbbafe3bbf0aa2fec5219b133f29b2a5fe"
-
-  patch do
-    # Ensure pathlimit argument passed along to somatic and denovo commands
-    # Ensure reference passed to export commands
-    url "https://gist.githubusercontent.com/chapmanb/43e363680223656440de/raw/406a5efaba5d3fc0ed341a7daa2e30abc0228112/scalpel_pathlimit.patch"
-    sha1 "771437c4b83a25ad286c1d1013bb0b2358ca35e2"
-  end
+  version '0.3.1_4-e51eb1'
+  url "https://s3.amazonaws.com/cloudbiolinux/cache/scalpel-code-e51eb12a2cc95326be31d3a1f7b92b9b63d2e39c.zip"
+  sha1 "594c3df366be1582f9b2845861a089fde5e59b23"
 
   def install
     ENV.deparallelize
-    inreplace 'ExportVariants.pl' do |s|
-      # Remove incorrect filter specification which causes parsing issues with GATK
-      s.sub! "ID=MS,Number=1,Type=STRING", "ID=MS"
-      s.sub! "ID=lowCov,Number=0,Type=flag", "ID=lowCov"
-    end
     inreplace 'FindVariants.pl' do |s|
       # Use samtools by chromosome to avoid pulling the full genome into memory
       s.sub! 'if (-e "$bedfile") { $USEFAIDX = 0; }', 'if (-e "$bedfile") { $USEFAIDX = 1; }'
