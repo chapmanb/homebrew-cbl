b11b03efd5c5f11f309666776340480336c223c8
chapmanb <chapmanb@50mail.com>
2015-02-04 04:10:21 -0500

    sambamba: Move to 0.5.2a pre-release to avoid memory leak issues (lomereiter/sambamba#116 chapmanb/bcbio-nextgen#741)

--- sambamba-binary.rb
+++ sambamba-binary.rb
@@ -2,17 +2,19 @@ require 'formula'
 
 class SambambaBinary < Formula
   homepage 'https://github.com/lomereiter/sambamba'
-  version '0.5.1'
+  version '0.5.2a-2015_02_02'
   if OS.mac?
     url "https://github.com/lomereiter/sambamba/releases/download/v#{version}/sambamba_v#{version}_osx.tar.bz2"
     sha1 "5d916fc1314913557988382822b9f9f070795ca5"
   else
-    url "https://github.com/lomereiter/sambamba/releases/download/v#{version}/sambamba_v#{version}_linux.tar.bz2"
-    sha1 "bc7a02d2b377782593f2c06c6f3550417a9984cb"
+    url "https://dl.dropboxusercontent.com/u/7916095/sambamba_02_02_2015"
+    #url "https://github.com/lomereiter/sambamba/releases/download/v#{version}/sambamba_v#{version}_linux.tar.bz2"
+    sha1 "4906ce721fa48273738eae34e630f0c9ca65f2bc"
   end
 
   def install
-    bin.install "sambamba_v#{version}" => 'sambamba'
+    bin.install "sambamba_02_02_2015" => "sambamba"
+    #bin.install "sambamba_v#{version}" => "sambamba"
   end
 
   test do
