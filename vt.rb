2e27de109776ccb569eabc39e2cad2205c9f73bf
chapmanb <chapmanb@50mail.com>
2015-02-12 10:45:12 -0500

    vt: Add binary build target to enable use on system with older gcc versions

--- vt.rb
+++ vt.rb
@@ -6,9 +6,20 @@ class Vt < Formula
   url "https://github.com/atks/vt/archive/a632462.tar.gz"
   sha1 "1fc41946edc05cae5df44f8216c2339ea9d7157f"
 
+  option "with-binary", "Install a statically linked binary for 64-bit Linux" if OS.linux?
+
+  resource "vt-binary" do
+    url "https://s3.amazonaws.com/cloudbiolinux/binaries/vt-centos5-2015-02-10"
+    sha1 "87b7d1ab1cdf7df5a6807f46da1f7fe38705a773"
+  end
+
   def install
-    system 'make'
-    bin.install 'vt'
+    if build.with? "binary"
+      resource("vt-binary").stage { bin.install "vt-centos5-#{version}" => "vt"}
+    else
+      system 'make'
+      bin.install 'vt'
+    end
   end
 
   test do
