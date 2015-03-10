6641421beb2c7cd7db5d3a35f342f84f954d1c41
Miika Ahdesmaki <miika.ahdesmaki@live.fi>
2015-03-10 15:15:46 +0000

    Pull latest VarDict bug fixes in

--- vardict.rb
+++ vardict.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Vardict < Formula
   homepage 'https://github.com/AstraZeneca-NGS/VarDict'
-  version '2015-01-16'
-  url 'https://github.com/AstraZeneca-NGS/VarDict.git', :revision => '53a80fa4d'
+  version '2015-03-10'
+  url 'https://github.com/AstraZeneca-NGS/VarDict.git', :revision => '75496948d'
 
   def install
     prefix.install Dir['*']
     bin.install_symlink prefix / 'vardict.pl' => 'vardict'
     bin.install_symlink(['vardict.pl', 'testsomatic.R', 'teststrandbias.R',
-                         'var2vcf_valid.pl', 'var2vcf_paired.pl', 'var2vcf_somatic.pl'].map do
+                         'var2vcf_valid.pl', 'var2vcf_paired.pl'].map do
                           |x| prefix / x
     end)
   end
