458f76aa862789fa4a2dcdb4fe4f3babb22d880a
chapmanb <chapmanb@50mail.com>
2015-03-18 10:01:12 -0400

    sambamba: update version with fix to avoid temporary file cleanup errors with busy NFS shares chapmanb/bcbio-nextgen#784 lomereiter/sambamba#124

--- sambamba-binary.rb
+++ sambamba-binary.rb
@@ -2,19 +2,17 @@ require 'formula'
 
 class SambambaBinary < Formula
   homepage 'https://github.com/lomereiter/sambamba'
-  version '0.5.2a-2015-02-02'
+  version '0.5.2a-2015-03-18'
   if OS.mac?
     url "https://github.com/lomereiter/sambamba/releases/download/v#{version}/sambamba_v#{version}_osx.tar.bz2"
     sha1 "5d916fc1314913557988382822b9f9f070795ca5"
   else
-    url "https://s3.amazonaws.com/cloudbiolinux/cache/sambamba_02_02_2015"
-    #url "https://github.com/lomereiter/sambamba/releases/download/v#{version}/sambamba_v#{version}_linux.tar.bz2"
-    sha1 "4906ce721fa48273738eae34e630f0c9ca65f2bc"
+    url "https://s3.amazonaws.com/cloudbiolinux/cache/sambamba_0.5.2a-2015-03-18"
+    sha1 "b3f9628e41956c0e4c0f49976f6bc406514efa97"
   end
 
   def install
-    bin.install "sambamba_02_02_2015" => "sambamba"
-    #bin.install "sambamba_v#{version}" => "sambamba"
+    bin.install "sambamba_#{version}" => "sambamba"
   end
 
   test do
