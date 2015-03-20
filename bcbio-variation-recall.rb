d220d5eef2a22db6149319d82cb5e0192d493add
chapmanb <chapmanb@50mail.com>
2015-03-20 04:51:46 -0400

    bcbio-variation-recall: v0.0.8

--- bcbio-variation-recall.rb
+++ bcbio-variation-recall.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class BcbioVariationRecall < Formula
   homepage "https://github.com/chapmanb/bcbio.variation.recall/"
-  version "0.0.7"
+  version "0.0.8"
   url "https://github.com/chapmanb/bcbio.variation.recall/releases/download/v#{version}/bcbio-variation-recall"
-  sha1 "73bff0e8e25f0d8acf9e24fc706a36c2d14751d9"
+  sha1 "b8dc414a18e01aa3056ad2060bf9c8b7b9bc0932"
 
   def install
     bin.install "bcbio-variation-recall"
