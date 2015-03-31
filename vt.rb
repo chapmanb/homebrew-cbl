bd27db74d07974584b3046f7f95b06c96dbee61f
chapmanb <chapmanb@50mail.com>
2015-03-31 05:43:49 -0400

    vt: 2015-03-26 version with fixes for normalization

--- vt.rb
+++ vt.rb
@@ -2,20 +2,20 @@ require 'formula'
 
 class Vt < Formula
   homepage 'https://github.com/atks/vt'
-  version '2015-03-12'
-  url "https://github.com/atks/vt/archive/10b7b2b.tar.gz"
-  sha1 "d56ec15c38db4339a041a030a8db5f5216f2e981"
+  version '2015-03-26'
+  url "https://github.com/atks/vt/archive/00a4e00.tar.gz"
+  sha1 "07353e048f28d768ae80424d4553d803c224cce2"
 
   option "with-binary", "Install a statically linked binary for 64-bit Linux" if OS.linux?
 
   resource "vt-binary" do
-    url "https://s3.amazonaws.com/cloudbiolinux/binaries/vt-centos5-2015.03.12"
-    sha1 "ba3060c4074fa8f9b4d6d0ce38730ee856e1dad8"
+    url "https://s3.amazonaws.com/cloudbiolinux/binaries/vt-centos5-2015.03.26"
+    sha1 "657cc356ceeb4307bd70bf3a30e5117dc0ed76fb"
   end
 
   def install
     if build.with? "binary"
-      resource("vt-binary").stage { bin.install "vt-centos5-2015.03.12" => "vt"}
+      resource("vt-binary").stage { bin.install "vt-centos5-2015.03.26" => "vt"}
     else
       system 'make'
       bin.install 'vt'
