912f0b13a412156652ae9e31322e2f78dc1abb79
chapmanb <chapmanb@50mail.com>
2015-04-02 05:44:53 -0400

    bcbio.variation.recall: 0.0.9 with fixes for new FreeBayes

--- bcbio-variation-recall.rb
+++ bcbio-variation-recall.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class BcbioVariationRecall < Formula
   homepage "https://github.com/chapmanb/bcbio.variation.recall/"
-  version "0.0.8"
+  version "0.0.9"
   url "https://github.com/chapmanb/bcbio.variation.recall/releases/download/v#{version}/bcbio-variation-recall"
-  sha1 "b8dc414a18e01aa3056ad2060bf9c8b7b9bc0932"
+  sha1 "af2a21e7d79228336e05e731cfe1e4a9e8859b48"
 
   def install
     bin.install "bcbio-variation-recall"
