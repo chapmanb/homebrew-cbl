fb05f8f182c730add22b635ced0834b02b7b02a2
Torsten Seemann <torsten.seemann@gmail.com>
2015-04-02 16:09:00 +1100

    Skewer 0.1.123 (update)

--- skewer.rb
+++ skewer.rb
@@ -1,12 +1,14 @@
-require 'formula'
-
 class Skewer < Formula
-  homepage 'http://sourceforge.net/projects/skewer/'
-  version '0.1.117'
-  url "http://downloads.sourceforge.net/project/skewer/Binaries/skewer-#{version}-linux-x86_64"
-  sha1 '05fe185d19a43f23a3b6486ce813ee9068d47e72'
+  homepage "http://sourceforge.net/projects/skewer/"
+  version "0.1.123"
+  url "https://downloads.sourceforge.net/project/skewer/Binaries/skewer-#{version}-linux-x86_64"
+  sha256 "a188508269ff55c88f73277ac21474ad521bbfed83ae8810b773290db22a7880"
 
   def install
-    bin.install "skewer-#{version}-linux-x86_64" => 'skewer'
+    bin.install "skewer-#{version}-linux-x86_64" => "skewer"
+  end
+
+  test do
+    system "#{bin}/skewer --help"
   end
 end
