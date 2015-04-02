57636f1cc2183615e331cd6907fe57351b8dcef9
Torsten Seemann <torsten.seemann@gmail.com>
2015-04-02 18:30:14 +1100

    Tophat 2.0.14 (upgrade)

--- tophat-binary.rb
+++ tophat-binary.rb
@@ -1,15 +1,14 @@
-require 'formula'
 
 class TophatBinary < Formula
-  homepage 'http://ccb.jhu.edu/software/tophat'
-  version '2.0.13'
+  homepage "http://ccb.jhu.edu/software/tophat"
+  version "2.0.14"
 
   if OS.mac?
-    url "http://ccb.jhu.edu/software/tophat/downloads/tophat-2.0.13.OSX_x86_64.tar.gz"
-    sha1 'b40cdc6442089d0cf22387bf18d64abf43fe279f'
+    url "http://ccb.jhu.edu/software/tophat/downloads/tophat-#{version}.OSX_x86_64.tar.gz"
+    sha256 "aa57a2f40b0813b4404d3b462db0ad54f93f6a2e71a826541dd15e04cda75d09"
   else
-    url "http://ccb.jhu.edu/software/tophat/downloads/tophat-2.0.13.Linux_x86_64.tar.gz"
-    sha1 'e313f188b97f143ec7c3a9ed9c7a702e0463ea24'
+    url "http://ccb.jhu.edu/software/tophat/downloads/tophat-#{version}.Linux_x86_64.tar.gz"
+    sha256 "273894525b81052537f1a8cb464a7eb7ffbef78e2f34b0ac2b22c3964a7c9748"
   end
 
   def install
