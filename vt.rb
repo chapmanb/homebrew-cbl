c013f182223a070f2108c2ff0eb4112d37a11506
chapmanb <chapmanb@50mail.com>
2015-07-04 00:49:38 -0400

    vt: update to latest version with fixes for decomposition ID creation

--- vt.rb
+++ vt.rb
@@ -2,20 +2,20 @@ require 'formula'
 
 class Vt < Formula
   homepage 'https://github.com/atks/vt'
-  version '2015-03-26'
-  url "https://github.com/atks/vt/archive/00a4e00.tar.gz"
-  sha1 "07353e048f28d768ae80424d4553d803c224cce2"
+  version '2015-07-02'
+  url "https://github.com/atks/vt/archive/e21fe8f.tar.gz"
+  sha1 "5385ec9fdad65e3398885c32ca128b7fd137519b"
 
   option "with-binary", "Install a statically linked binary for 64-bit Linux" if OS.linux?
 
   resource "vt-binary" do
-    url "https://s3.amazonaws.com/cloudbiolinux/binaries/vt-centos5-2015.03.26"
-    sha1 "657cc356ceeb4307bd70bf3a30e5117dc0ed76fb"
+    url "https://s3.amazonaws.com/cloudbiolinux/binaries/vt-centos5-2015.07.02"
+    sha1 "45a34cdf253f08ae88048b4a035ffa4bf65977ed"
   end
 
   def install
     if build.with? "binary"
-      resource("vt-binary").stage { bin.install "vt-centos5-2015.03.26" => "vt"}
+      resource("vt-binary").stage { bin.install "vt-centos5-2015.07.02" => "vt"}
     else
       system 'make'
       bin.install 'vt'
