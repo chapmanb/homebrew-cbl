ea17a5ba695f23d6166f4454a1bcae11e34d9148
chapmanb <chapmanb@50mail.com>
2015-07-31 11:57:56 -0400

    sambamba: update to 0.5.6

--- sambamba-binary.rb
+++ sambamba-binary.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class SambambaBinary < Formula
   homepage 'https://github.com/lomereiter/sambamba'
-  version '0.5.2'
+  version '0.5.6'
   if OS.mac?
     url "https://github.com/lomereiter/sambamba/releases/download/v#{version}/sambamba_v#{version}_osx.tar.bz2"
-    sha1 "caf3c1259cd30acc1b71470334f77a2bcb1170a2"
+    sha256 "a61e71fb09ac9d4eeacc79d58d388b79b296e788663bd315633d25f5134196e6"
   else
     url "https://github.com/lomereiter/sambamba/releases/download/v#{version}/sambamba_v#{version}_linux.tar.bz2"
-    sha1 "bc195de83029a55d0f9a8abfb200a674584c8fd2"
+    sha256 "5053472f33fb40b92489268b1de186143198616335911d8fd016ea7a63424f6a"
   end
 
   def install
