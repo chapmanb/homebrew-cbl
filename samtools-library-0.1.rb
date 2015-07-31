9d0160ce64eb14ac9123514b7d0385aa891ef67e
chapmanb <chapmanb@50mail.com>
2015-07-31 13:30:07 -0400

    samtools-0.1-library: remove curses dependency
    curses can break installs on SUSE systems and is not required for
    this library-based build.

--- samtools-library-0.1.rb
+++ samtools-library-0.1.rb
@@ -9,6 +9,10 @@ class SamtoolsLibrary01 < Formula
   sha256 "500019b4d1886ad995513d5ce8b413b14b51f16f251ce76bc0b35a446b182a47"
 
   def install
+    inreplace "Makefile" do |s|
+      s.gsub! "-D_CURSES_LIB=1", "-D_CURSES_LIB=0"
+      s.gsub! "-lcurses", "# -lcurses"
+    end
     system "make CFLAGS='-g -Wall -O2 -fPIC'"
     (share+"samtools-0.1"+"lib").install "libbam.a"
     (share+"samtools-0.1"+"include").install Dir["*.h"]
