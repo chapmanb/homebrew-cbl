36cd13b007be832d7b41822124757cc0538afa6b
chapmanb <chapmanb@50mail.com>
2015-08-01 04:57:42 -0400

    manta: update to 0.27.0, do not install large demo data

--- manta.rb
+++ manta.rb
@@ -11,9 +11,9 @@ class Manta < Formula
   desc "Long-read alignment with optimal breakpoint detection"
   homepage "https://github.com/Illumina/manta"
 
-  version "0.26.5"
+  version "0.27.0"
   url "https://github.com/Illumina/manta/releases/download/v#{version}/manta-#{version}.centos5_x86_64.tar.bz2"
-  sha256 "ef58cc6955355c5b63b3a6181d9db3448d0399f19aab764980fa8ecd0351958d"
+  sha256 "40c06e42676b3c37c568dfc342efbea17f625f17d2ae8ab2f0a2e6e2bb6b709a"
 
   depends_on LinuxRequirement
 
@@ -22,6 +22,7 @@ class Manta < Formula
       s.gsub! "__file__", "os.path.realpath(__file__)"
     end
     prefix.install Dir["*"]
+    rm_rf prefix/"share/demo"
   end
 
   test do
