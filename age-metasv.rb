7a231448366c0137672258d0f0e4c3dc65f36fe2
chapmanb <chapmanb@50mail.com>
2015-08-07 11:50:45 -0400

    AGE: provide pre-compiled binary install default

--- age-metasv.rb
+++ age-metasv.rb
@@ -6,9 +6,20 @@ class AgeMetasv < Formula
   url "https://github.com/marghoob/AGE/archive/344429ccb.tar.gz"
   sha256 "ca9f1072d3cd91858034001b4299a2ea98a7a8930d785bec2aadacbec3dbe523"
 
+  option "without-binary", "Do not use a statically linked binary for 64-bit Linux" if OS.linux?
+
+  resource "age-binary" do
+    url "https://s3.amazonaws.com/cloudbiolinux/binaries/age_align-centos5-2015.01.29"
+    sha256 "97231e32befbce30c6988a64962a141a2a4cbd3d22a65de6e7462076cfdfd2d3"
+  end
+
   def install
-    system "make"
-    bin.install "age_align"
+    if build.with? "binary"
+      resource("age-binary").stage { bin.install "age_align-centos5-2015.01.29" => "age_align"}
+    else
+      system "make"
+      bin.install "age_align"
+    end
   end
 
   test do
