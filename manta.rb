fb1119645d8da39a38d8186ad1fa6b2060905fc0
chapmanb <chapmanb@50mail.com>
2015-08-09 05:45:20 -0400

    manta: update to 0.27.1

--- manta.rb
+++ manta.rb
@@ -7,13 +7,12 @@ class LinuxRequirement < Requirement
 end
 
 class Manta < Formula
-  desc " Structural variant and indel caller for mapped sequencing data"
-  desc "Long-read alignment with optimal breakpoint detection"
+  desc "Structural variant and indel caller for mapped sequencing data"
   homepage "https://github.com/Illumina/manta"
 
-  version "0.27.0"
+  version "0.27.1"
   url "https://github.com/Illumina/manta/releases/download/v#{version}/manta-#{version}.centos5_x86_64.tar.bz2"
-  sha256 "40c06e42676b3c37c568dfc342efbea17f625f17d2ae8ab2f0a2e6e2bb6b709a"
+  sha256 "788a0a184168ed91bd3cf0c44dda6b9186fd9562476203b8e92b20e963f48769"
 
   depends_on LinuxRequirement
 
