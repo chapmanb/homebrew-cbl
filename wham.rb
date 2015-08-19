5af8708f6d3345ed49a9ace0c83e547892613bbc
chapmanb <chapmanb@50mail.com>
2015-08-19 05:18:37 -0400

    WHAM: latest development with WHAM-GRAPHENING

--- wham.rb
+++ wham.rb
@@ -20,26 +20,30 @@ end
 
 class Wham < Formula
   homepage "https://github.com/jewmanchue/wham"
-  version "1.7.0-60"
+  version "1.7.0-131"
 
   resource "wham-binary" do
-    url "https://s3.amazonaws.com/cloudbiolinux/binaries/WHAM-BAM-1.7.0-60"
-    sha1 "b664f1cdee7242762504efcd6131fa317c1edfc1"
+    url "https://s3.amazonaws.com/cloudbiolinux/binaries/WHAM-binaries-1.7.0-131.tar.gz"
+    sha256 "3c3882b2778fd5c960180b7f6e20a5e61c8457aaee9003388107872f52c5accd"
   end
 
   option "with-binary", "Install a statically linked binary for 64-bit Linux" if OS.linux?
 
   if build.without? "binary"
-    url 'https://github.com/jewmanchue/wham.git', :using => RecursiveGitDownloadStrategy, :revision => "2a1f349"
+    url 'https://github.com/jewmanchue/wham.git', :using => RecursiveGitDownloadStrategy, :revision => "60b9768"
     depends_on "bamtools"
   else
-    url "https://github.com/jewmanchue/wham/archive/2a1f349.tar.gz"
-    sha1 "93d31cb803cb6b3b1b21b6d22e233c29a9c39383"
+    url "https://github.com/jewmanchue/wham/archive/60b9768.tar.gz"
+    sha256 "d441e87d3955d744faa93d2839e50b85cba8b45cf4754b2326d23ee9fbb77e7b"
   end
 
   def install
     if build.with? "binary"
-      resource("wham-binary").stage { bin.install "WHAM-BAM-1.7.0-60" => "WHAM-BAM"}
+      resource("wham-binary").stage {
+        bin.install "WHAM-BAM"
+        bin.install "WHAM-GRAPHENING"
+        bin.install "mergeIndvs"
+      }
     else
       inreplace "Makefile" do |s|
         # Link bamtools dependency to external brew package
@@ -51,6 +55,8 @@ class Wham < Formula
       end
       system "make"
       bin.install "bin/WHAM-BAM"
+      bin.install "bin/WHAM-GRAPHENING"
+      bin.install "bin/mergeIndvs"
     end
     (share/"wham").install "data/WHAM_training_data.txt"
     (share/"wham").install "utils/classify_WHAM_vcf.py"
