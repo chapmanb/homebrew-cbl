ce602c7cc5f5c596ba95e64670f22f239fae948b
chapmanb <chapmanb@50mail.com>
2015-08-31 10:06:02 +0100

    scalpel: fix handling of external bcftools
    - Correctly link to external homebrew bcftools. Fixes chapmanb/bcbio-nextgen#989
    - Use external samtools and bcftools to avoid building in formula.

--- scalpel.rb
+++ scalpel.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Scalpel < Formula
   homepage 'http://scalpel.sourceforge.net/'
-  version '0.3.1_5-2d23b7'
+  version '0.3.1_5-2d23b7-2'
   url "https://s3.amazonaws.com/cloudbiolinux/cache/scalpel-code-2d23b7192228894d97fc6d8ffe4bc1b0618c9861.zip"
   sha1 "7e584a338ee96e2802acdac4092aef43529d2697"
 
@@ -19,18 +19,22 @@ class Scalpel < Formula
     inreplace 'Utils.pm' do |s|
       # Use system bamtools instead of local, which doesn't load libbamtools
       s.gsub! "$Bin/bamtools-2.3.0/bin/", ""
+      # Use system samtools and bcftools
+      s.gsub! "$Bin/samtools-1.1/", ""
+      s.gsub! "$Bin/bcftools-1.1/", ""
       # Ensure local scripts point to installation
       s.gsub! '$Bin/', "#{prefix}/"
       # Use bash instead of /bin/sh default, which points to dash on ubuntu
       s.sub! 'system($cmd)', 'system("/bin/bash -c \'$cmd\'")'
     end
     inreplace 'Makefile', 'CXX := g++', "CXX := #{ENV.cxx}"
-    # Avoid building bamtools
-    inreplace "Makefile", "bamtools samtools", "samtools"
+    # Avoid building bamtools samtools bcftools
+    inreplace "Makefile", "bamtools samtools bcftools", ""
     chmod 0755, 'FindVariants.pl'
     chmod 0755, 'FindSomatic.pl'
-    system 'make Microassembler'
-    system 'make'
+    bamtools=Formula["bamtools"].opt_prefix
+    system "make INCLUDES='-I#{bamtools}/include/bamtools -L #{bamtools}/lib' Microassembler"
+    system "make INCLUDES='-I#{bamtools}/include/bamtools -L #{bamtools}/lib'"
     prefix.install Dir['*']
     bin.install_symlink prefix / 'scalpel'
   end
