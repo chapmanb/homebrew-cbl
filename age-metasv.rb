4ef526d442d9c37ab73a963bdc5e8837ae78c265
chapmanb <chapmanb@50mail.com>
2015-09-02 15:44:36 -0400

    AGE: pre-build without -static -O3 which cause it to hang on CentOS

--- age-metasv.rb
+++ age-metasv.rb
@@ -2,20 +2,20 @@ require 'formula'
 
 class AgeMetasv < Formula
   homepage "https://github.com/marghoob/AGE/tree/simple-parseable-output"
-  version "2015-01-29-2"
+  version "2015-01-29-3"
   url "https://github.com/marghoob/AGE/archive/344429ccb.tar.gz"
   sha256 "ca9f1072d3cd91858034001b4299a2ea98a7a8930d785bec2aadacbec3dbe523"
 
   option "without-binary", "Do not use a statically linked binary for 64-bit Linux" if OS.linux?
 
   resource "age-binary" do
-    url "https://s3.amazonaws.com/cloudbiolinux/binaries/age_align-centos5-2015.01.29.2"
-    sha256 "b249a9299f4f36f93b08d03c58226f4d7ebb7630183387accc475550bc8f1bd8"
+    url "https://s3.amazonaws.com/cloudbiolinux/binaries/age_align-centos5-2015.01.29.3"
+    sha256 "a217b8607c972b9ee18d2c603997fe32b8d1d122aec4d32acda4c7bfa8bca0fb"
   end
 
   def install
     if build.with? "binary"
-      resource("age-binary").stage { bin.install "age_align-centos5-2015.01.29.2" => "age_align"}
+      resource("age-binary").stage { bin.install "age_align-centos5-2015.01.29.3" => "age_align"}
     else
       system "make"
       bin.install "age_align"
