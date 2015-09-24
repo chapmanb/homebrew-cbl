2eec881a84e94880a8febbfea9ddd761ef1fe9fa
roryk <rory.kirchner@gmail.com>
2015-09-24 17:30:55 -0400

    Update sambamba to 0.5.8.
    This has some fixes to the depth calculations.

--- sambamba-binary.rb
+++ sambamba-binary.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class SambambaBinary < Formula
   homepage 'https://github.com/lomereiter/sambamba'
-  version '0.5.6'
+  version '0.5.8'
   if OS.mac?
     url "https://github.com/lomereiter/sambamba/releases/download/v#{version}/sambamba_v#{version}_osx.tar.bz2"
-    sha256 "a61e71fb09ac9d4eeacc79d58d388b79b296e788663bd315633d25f5134196e6"
+    sha256 "43acd0f5d7a88ca4b559527ffc4456d2e66d29e095478b626c73c287d81b775c"
   else
     url "https://github.com/lomereiter/sambamba/releases/download/v#{version}/sambamba_v#{version}_linux.tar.bz2"
-    sha256 "5053472f33fb40b92489268b1de186143198616335911d8fd016ea7a63424f6a"
+    sha256 "6badcdfa66dcdc914fe843e9893185cfb595704f3ad3058decf4c288360fb82b"
   end
 
   def install
