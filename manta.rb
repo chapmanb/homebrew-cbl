8dbb44e4c524d05a404ef2df28849e09c150145a
chapmanb <chapmanb@50mail.com>
2015-10-02 06:04:28 -0400

    manta: update to 0.29.0 with hg38 support

--- manta.rb
+++ manta.rb
@@ -10,9 +10,9 @@ class Manta < Formula
   desc "Structural variant and indel caller for mapped sequencing data"
   homepage "https://github.com/Illumina/manta"
 
-  version "0.28.0"
+  version "0.29.0"
   url "https://github.com/Illumina/manta/releases/download/v#{version}/manta-#{version}.centos5_x86_64.tar.bz2"
-  sha256 "aef97bc887cbfca8cfe2e3a076c52bc95b8ecd37f1586b433211cacfcce14ae0"
+  sha256 "7dd9f2c88f9f918539c32ce1a4d60ad6ae53c0a2c609714330bd411975ec6a2d"
 
   depends_on LinuxRequirement
 
