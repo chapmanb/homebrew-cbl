2f7c5036600cf9d7ed68fa467a4583a4c45c3dd7
chapmanb <chapmanb@50mail.com>
2015-10-15 06:22:37 -0400

    bcbio-variation-recall: 0.1.4 with caller annotations
    Adds support for annotating callers supporting an ensemble
    call (chapmanb/bcbio-nextgen#1018)

--- bcbio-variation-recall.rb
+++ bcbio-variation-recall.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class BcbioVariationRecall < Formula
   homepage "https://github.com/chapmanb/bcbio.variation.recall/"
-  version "0.1.2"
+  version "0.1.4"
   url "https://github.com/chapmanb/bcbio.variation.recall/releases/download/v#{version}/bcbio-variation-recall"
-  sha1 "a581da94867499f652619e5d7d0245f443cc8ae1"
+  sha256 "9551f6cbb4ac0ae39e6dfa802fa5bb4279e59b9a3fad944a021bf661c634e4ed"
 
   def install
     bin.install "bcbio-variation-recall"
