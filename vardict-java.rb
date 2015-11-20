07a2843cc50dd0c14dfd0d479d547dd6ccd79033
Miika Ahdesmaki <miika.ahdesmaki@live.fi>
2015-11-20 14:54:21 +0000

    Version bumped properly...

--- vardict-java.rb
+++ vardict-java.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class VardictJava < Formula
   homepage "https://github.com/AstraZeneca-NGS/VarDictJava"
-  version "1.4.1"
+  version "1.4.2"
   url "https://github.com/AstraZeneca-NGS/VarDictJava/releases/download/v1.4.2/VarDict-1.4.2.zip"
   sha256 "df9940dd1dc281848e9b0951659c130a891b8ab61161c771f5ceda7c5df874a0"
 
