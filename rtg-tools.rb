2de956c2ef3abd165c0d00004bb9d4d5c5e8d0ef
Len Trigg <len@realtimegenomics.com>
2016-08-26 10:44:51 +1200

    rtg-tools: update to new release, 3.7

--- rtg-tools.rb
+++ rtg-tools.rb
@@ -2,20 +2,22 @@ require 'formula'
 
 class RtgTools < Formula
   homepage "http://realtimegenomics.com/products/rtg-tools/"
-  version "3.6.2"
+  version "3.7"
 
-  url "https://github.com/RealTimeGenomics/rtg-tools/releases/download/3.6.2/rtg-tools-3.6.2-nojre.zip"
-  sha1 "f8feb5d1e572ffd4b241413bfa51929bf56be026"
+  url "https://github.com/RealTimeGenomics/rtg-tools/releases/download/3.7/rtg-tools-3.7-nojre.zip"
+  sha1 "623fbe5d5f715750b7a86c0c00618fb1580ce76a"
 
   def install
     java = share / 'java'
     java.install 'RTG.jar'
     doc.install 'RTGOperationsManual.pdf'
+    doc.install 'RTGOperationsManual'
     prefix.install 'LICENSE.txt'
     prefix.install 'README.txt'
     prefix.install 'ReleaseNotes.txt'
     prefix.install 'rtg'
     prefix.install 'third-party'
+    prefix.install 'scripts'
     open(prefix / 'rtg.cfg', 'w') do |file|
       file.write <<-EOS.undent
         RTG_TALKBACK=     # Attempt to send crash logs to realtime genomics, true to enable
